54be31359e06422106f64d01760eef73408c524f
123anjuan <161805557+123anjuan@users.noreply.github.com>
2024-03-07 21:43:04 +0800

    Update and rename 00.figure6_GWAS.R to 00.step0.R

--- block 6/00.step0.R
+++ block 6/00.step0.R
@@ -34,15 +34,7 @@ for (i in 1:length(markerList)){
 }
 
 
-#Step 1: Transform single cell Atac-seq peaks bed files in from hg38 to hg19
-for i in {1..22}
-	do
-	/liftOver \
-     ./differtest_peak_$i.txt \
-     hg38ToHg19.over.chain.gz \
-     differtest$i.hg38_to_hg19.txt \
-     differtest$i.unlifted.txt
-    done
+
 
 #Step 2: Creating an annot file------------------------source activate ldsc------------------
 for i in {1..22}
